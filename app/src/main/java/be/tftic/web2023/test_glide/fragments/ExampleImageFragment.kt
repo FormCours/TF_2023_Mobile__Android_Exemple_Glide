63a804a2ef48201365db2ee5d864decb843e5a86
Aurélien <aurelien.strimelle@bstorm.be>
2023-08-09 14:10:17 +0200

    Demo Glide

--- app/src/main/java/be/tftic/web2023/test_glide/fragments/ExampleImageFragment.kt
+++ app/src/main/java/be/tftic/web2023/test_glide/fragments/ExampleImageFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import be.tftic.web2023.test_glide.R
 import be.tftic.web2023.test_glide.databinding.FragmentExampleImageBinding
+import com.bumptech.glide.Glide
 
 
 class ExampleImageFragment : Fragment() {
@@ -18,6 +20,13 @@ class ExampleImageFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentExampleImageBinding.inflate(inflater, container, false);
+
+        // Loading image with Glide
+        Glide.with(this)
+            .load("https://www.pokepedia.fr/images/a/a0/Pichu-RFVF.png")
+            .placeholder(R.drawable.not_loading)
+            .fitCenter()
+            .into(binding.imgFragExample);
 
         // Return View
         return binding.root
